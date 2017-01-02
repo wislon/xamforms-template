1ca4f6ecd54992632c464d4ba2b91237c60a7b3c
John Wilson <wislon@gmail.com>
2017-01-02 19:48:02 +1100

    ios, droid & shared - install akavavache 5.0 and dependencies (rx, sqlite)

--- src/XamForms/XamForms.Droid/Resources/Resource.Designer.cs
+++ src/XamForms/XamForms.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace XamForms.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Splat.Resource.String.library_name = global::XamForms.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XamForms.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3002,6 +3003,9 @@ namespace XamForms.Droid
 			
 			// aapt resource value: 0x7f080025
 			public const int character_counter_pattern = 2131230757;
+			
+			// aapt resource value: 0x7f080026
+			public const int library_name = 2131230758;
 			
 			// aapt resource value: 0x7f080000
 			public const int mr_button_content_description = 2131230720;
