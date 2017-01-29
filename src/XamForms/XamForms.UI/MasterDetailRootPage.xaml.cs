f19aa614230c2b30d65edbe50987ec51a9282ba7
John Wilson <wislon@gmail.com>
2017-01-29 10:34:32 +1000

    add first cut of poc MasterDetail Root Page

---
+++ src/XamForms/XamForms.UI/MasterDetailRootPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using XamForms.UI.ViewModels;
+
+namespace XamForms.UI
+{
+  public partial class MasterDetailRootPage : MasterDetailPage
+  {
+    public MasterDetailRootPage()
+    {
+      InitializeComponent();
+
+      listNav.ViewModel = new MasterListNavViewModel();
+      normalOne.ViewModel = new NormalOneViewModel();
+    }
+  }
+}
