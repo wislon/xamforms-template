3656b31710796fe1b09effc8c7346f5d9a360487
John Wilson <wislon@gmail.com>
2017-01-29 10:30:49 +1000

    add some proof of concept views for master-detail, child, modal

---
+++ src/XamForms/XamForms.UI/Views/Normal/NormalModalPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using Xamarin.Forms;
+using XamForms.UI.ViewModels;
+
+namespace XamForms.UI.Views.Normal
+{
+  public partial class NormalModalPage : ContentPage, IViewFor<NormalModalViewModel>
+  {
+    private NormalModalViewModel _vm;
+
+    public NormalModalViewModel ViewModel
+    {
+      get { return _vm; }
+      set
+      {
+        _vm = value;
+        BindingContext = _vm;
+      }
+    }
+
+    object IViewFor.ViewModel
+    {
+      get { return _vm; }
+      set
+      {
+        ViewModel = (NormalModalViewModel)value;
+      }
+    }
+
+    public NormalModalPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ src/XamForms/XamForms.UI/Views/Normal/NormalOnePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using Xamarin.Forms;
+using XamForms.UI.ViewModels;
+
+namespace XamForms.UI.Views.Normal
+{
+  public partial class NormalOnePage : ContentPage, IViewFor<NormalOneViewModel>
+  {
+    private NormalOneViewModel _vm;
+    public NormalOneViewModel ViewModel
+    {
+      get { return _vm; }
+      set
+      {
+        _vm = value;
+        BindingContext = _vm;
+      }
+    }
+
+    object IViewFor.ViewModel
+    {
+      get { return _vm; }
+      set { ViewModel = (NormalOneViewModel)value; }
+    }
+
+    public NormalOnePage()
+    {
+      InitializeComponent();
+    }
+  }
+}
