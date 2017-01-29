dc18b8331d4038eb2a4a74c5ac7fe2dbea73cc4a
John Wilson <wislon@gmail.com>
2017-01-29 10:27:41 +1000

    add some supporting classes for commands, behaviours, storing/displaying views etc.

---
+++ src/XamForms/XamForms.UI/Models/MasterListItem.cs
@@ -0,0 +1,15 @@
+﻿using XamForms.UI.Interfaces;
+using XamForms.UI.ViewModels;
+
+namespace XamForms.UI.Models
+{
+  public class MasterListItem<T> : IMasterListItem<T> where T : BaseViewModel
+  {
+    public string DisplayName { get; set; }
+
+    public MasterListItem(string displayName)
+    {
+      DisplayName = displayName;
+    }
+  }
+}
